461400241036c56c973be5f233d9cd8785446db1
Mikael Hakansson <wmmihaa@hotmail.com>
2023-02-27 10:54:44 +0000

    Updated init script

--- recipes-core/intel-nuc-init/files/intel-nuc-init.sh
+++ recipes-core/intel-nuc-init/files/intel-nuc-init.sh
@@ -6,13 +6,18 @@
 #
 #***********************************************************************
 
-# Get script name
-me="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
-
 RESULT=0
 
 # Set hostname to serial number
-/usr/sbin/dmidecode -s chassis-serial-number > /etc/hostname
+hostnamectl set-hostname $(/usr/sbin/dmidecode -s system-serial-number)
+#/usr/sbin/dmidecode -s system-serial-number > /etc/hostname
 
+DNS1="nameserver 8.8.8.8"
+DNS2="nameserver 8.8.4.4"
+RESOLVECONF="/etc/resolv.conf"
+
+grep -qxF "$DNS1" $RESOLVECONF || echo $DNS1 >> $RESOLVECONF
+grep -qxF "$DNS2" $RESOLVECONF || echo $DNS2 >> $RESOLVECONF
+
+systemd-notify --ready
 exit $RESULT
-
